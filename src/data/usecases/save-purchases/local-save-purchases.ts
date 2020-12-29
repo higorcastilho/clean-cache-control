8320c62a34c6c81e8660242b475429b16095e0a0
Higor Castilho <castilhohf@gmail.com>
2020-12-29 16:59:56 -0300

    refactor: move production code to their own folders and files

---
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -0,0 +1,9 @@
+import { CacheStore } from '../../protocols/cache'
+
+export class LocalSavePurchases {
+	constructor (private readonly cacheStore: CacheStore) {}
+
+	async save (): Promise<void> {
+		this.cacheStore.delete('purchases')
+	}
+}
