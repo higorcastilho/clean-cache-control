b0e2e524c7490953ce2dc79b5ea8231c61ebb778
Higor Castilho <castilhohf@gmail.com>
2021-01-06 05:03:46 -0300

    test: ensure LocalSavePurchases create cache with correct timestamp

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -1,11 +1,17 @@
 import { CacheStore } from '../../protocols/cache'
 import { SavePurchases } from '../../../domain/usecases'
 
-export class LocalSavePurchases implements SavePurchases{
-	constructor (private readonly cacheStore: CacheStore) {}
+export class LocalSavePurchases implements SavePurchases {
+	constructor (
+		private readonly cacheStore: CacheStore,
+		private readonly timestamp: Date
+	) {}
 
 	async save (purchases: Array<SavePurchases.Params>): Promise<void> {
 		this.cacheStore.delete('purchases')
-		this.cacheStore.insert('purchases', purchases)
+		this.cacheStore.insert('purchases', {
+			timestamp: this.timestamp,
+			value: purchases
+		})
 	}
 }
