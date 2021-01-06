5ca77c1789b0f434acb5217732fbcc9d0227597c
Higor Castilho <castilhohf@gmail.com>
2021-01-06 06:49:36 -0300

    test: ensure LocalLoadPurchases will not delete or insert cache upon creation

---
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -0,0 +1,60 @@
+import { LocalLoadPurchases } from './local-load-purchases'
+import { mockPurchases, CacheStoreSpy } from '../../tests'
+
+type SutTypes = {
+	sut: LocalLoadPurchases
+	cacheStore: CacheStoreSpy
+}
+
+const makeSut = (timestamp = new Date()):SutTypes => {
+	const cacheStore = new CacheStoreSpy()
+	const sut = new LocalLoadPurchases(cacheStore, timestamp)
+	return {
+		sut,
+		cacheStore
+	}
+}
+
+describe('LocalLoadPurchases', () => {
+	test('Should not delete or insert cache on sut.init', () => {
+		const { cacheStore }= makeSut()
+		expect(cacheStore.actions).toEqual([])
+	})
+
+	test('Should call delete with correct key', async () => {
+		const { sut, cacheStore } = makeSut()
+		await sut.save(mockPurchases())
+		expect(cacheStore.deleteKey).toBe('purchases')
+	})
+
+	test('Should not insert new cach if delete fails', async () => {
+		const { sut, cacheStore } = makeSut()
+		cacheStore.simulateDeleteError()
+		const promise = sut.save(mockPurchases())
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete])
+		await expect(promise).rejects.toThrow()
+	})
+
+	test('Should insert new cach if delete succeeds', async () => {
+		const timestamp = new Date()
+		const { sut, cacheStore } = makeSut(timestamp)
+		const purchases = mockPurchases()
+		const promise = sut.save(purchases)
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
+		expect(cacheStore.deleteKey).toBe('purchases')
+		expect(cacheStore.insertKey).toBe('purchases')
+		expect(cacheStore.insertValues).toEqual({
+			timestamp,
+			value: purchases
+		})
+		await expect(promise).resolves.toBeFalsy()
+	})
+
+	test('Should throw if insert throws', async () => {
+		const { sut, cacheStore } = makeSut()
+		cacheStore.simulateInsertError()
+		const promise = sut.save(mockPurchases())
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
+		await expect(promise).rejects.toThrow()
+	})
+})
