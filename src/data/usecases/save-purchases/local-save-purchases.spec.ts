1c590b15d70e23c36226ad8643c85c3ed6ccd275
Higor Castilho <castilhohf@gmail.com>
2021-01-06 05:15:02 -0300

    refactor: change Message by Action

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -18,7 +18,7 @@ const makeSut = (timestamp = new Date()):SutTypes => {
 describe('LocalSavePurchases', () => {
 	test('Should not delete or insert cache on sut.init', () => {
 		const { cacheStore }= makeSut()
-		expect(cacheStore.messages).toEqual([])
+		expect(cacheStore.actions).toEqual([])
 	})
 
 	test('Should call delete with correct key', async () => {
@@ -31,7 +31,7 @@ describe('LocalSavePurchases', () => {
 		const { sut, cacheStore } = makeSut()
 		cacheStore.simulateDeleteError()
 		const promise = sut.save(mockPurchases())
-		expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete])
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete])
 		await expect(promise).rejects.toThrow()
 	})
 
@@ -40,7 +40,7 @@ describe('LocalSavePurchases', () => {
 		const { sut, cacheStore } = makeSut(timestamp)
 		const purchases = mockPurchases()
 		const promise = sut.save(purchases)
-		expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert])
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
 		expect(cacheStore.deleteKey).toBe('purchases')
 		expect(cacheStore.insertKey).toBe('purchases')
 		expect(cacheStore.insertValues).toEqual({
@@ -54,7 +54,7 @@ describe('LocalSavePurchases', () => {
 		const { sut, cacheStore } = makeSut()
 		cacheStore.simulateInsertError()
 		const promise = sut.save(mockPurchases())
-		expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert])
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
 		await expect(promise).rejects.toThrow()
 	})
 })
