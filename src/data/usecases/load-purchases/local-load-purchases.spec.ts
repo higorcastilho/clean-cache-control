a833dbe5d973c056dab3dc6bc7b376b8af192fb7
Higor Castilho <castilhohf@gmail.com>
2021-01-06 07:01:04 -0300

    test: ensure LocalLoadPurchases calls cacheStore.fetch with correct key

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -1,5 +1,5 @@
 import { LocalLoadPurchases } from './local-load-purchases'
-import { mockPurchases, CacheStoreSpy } from '../../tests'
+import { CacheStoreSpy } from '../../tests'
 
 type SutTypes = {
 	sut: LocalLoadPurchases
@@ -21,40 +21,10 @@ describe('LocalLoadPurchases', () => {
 		expect(cacheStore.actions).toEqual([])
 	})
 
-	test('Should call delete with correct key', async () => {
-		const { sut, cacheStore } = makeSut()
-		await sut.save(mockPurchases())
-		expect(cacheStore.deleteKey).toBe('purchases')
-	})
-
-	test('Should not insert new cach if delete fails', async () => {
-		const { sut, cacheStore } = makeSut()
-		cacheStore.simulateDeleteError()
-		const promise = sut.save(mockPurchases())
-		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete])
-		await expect(promise).rejects.toThrow()
-	})
-
-	test('Should insert new cach if delete succeeds', async () => {
-		const timestamp = new Date()
-		const { sut, cacheStore } = makeSut(timestamp)
-		const purchases = mockPurchases()
-		const promise = sut.save(purchases)
-		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
-		expect(cacheStore.deleteKey).toBe('purchases')
-		expect(cacheStore.insertKey).toBe('purchases')
-		expect(cacheStore.insertValues).toEqual({
-			timestamp,
-			value: purchases
-		})
-		await expect(promise).resolves.toBeFalsy()
-	})
-
-	test('Should throw if insert throws', async () => {
-		const { sut, cacheStore } = makeSut()
-		cacheStore.simulateInsertError()
-		const promise = sut.save(mockPurchases())
-		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
-		await expect(promise).rejects.toThrow()
+	test('Should call correct key on load', async () => {
+		const { cacheStore, sut }= makeSut()
+		await sut.loadAll()
+		expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch])
+		expect(cacheStore.fetchKey).toBe('purchases')
 	})
 })
